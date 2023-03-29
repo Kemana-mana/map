750751b17b3ca91d528c1bafc184f4dc5b6ca5a7
John Doe <shellbd22@gmail.com>
2023-03-29 12:44:42 +0700

    a

--- client.lua
+++ client.lua
@@ -6,36 +6,13 @@ Citizen.CreateThread(function()
     SetMapZoomDataLevel(4, 22.3, 0.9, 0.08, 0.0, 0.0)
 end)
 
--- Citizen.CreateThread(function()
---     while true do
--- 		Citizen.Wait(1)
--- 		if IsPedOnFoot(GetPlayerPed(-1)) then 
--- 			SetRadarZoom(1100)
--- 		elseif IsPedInAnyVehicle(GetPlayerPed(-1), true) then
--- 			SetRadarZoom(1100)
--- 		end
---     end
--- end)
-
--- optimized
-
 Citizen.CreateThread(function()
-  while true do
-      
-      if InCar then
-          Citizen.Wait(200)
-      else
-          Citizen.Wait(5000)
-  end
-
-      InCar = false
-
-      ped = PlayerPedId()
-      local isInCar = IsPedInAnyVehicle(ped, true)
-
-  if isInCar then
-          InCar = true
-    SetRadarZoom(1100)
-  end
-  end
+    while true do
+		Citizen.Wait(1)
+		if IsPedOnFoot(GetPlayerPed(-1)) then 
+			SetRadarZoom(1100)
+		elseif IsPedInAnyVehicle(GetPlayerPed(-1), true) then
+			SetRadarZoom(1100)
+		end
+    end
 end)
